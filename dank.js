329f1d19fba3603dddb5264d0b874e279233d171
ChrisCuevas <ccuevas@wisc.edu>
2015-07-09 22:59:29 -0700

    fixed error in js

--- dank.js
+++ dank.js
@@ -1,3 +1,4 @@
 document.body.innerHTML = document.body.replace(new RegExp("cool", "i"), "dank");
 document.body.innerHTML = document.body.replace(new RegExp("awesome", "i"), "dank");
-document.body.innerHTML = document.body.replace(new RegExp("amazing", "i"), "dank);`
+document.body.innerHTML = document.body.replace(new RegExp("amazing", "i"), "dank");
+document.body.innerHTML = document.body.replace(new RegExp("and", "i"), "dank");
