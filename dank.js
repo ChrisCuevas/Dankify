f382dc00fab305970c9819388c48bb2617cc0231
ChrisCuevas <ccuevas@wisc.edu>
2015-07-09 23:13:00 -0700

    Fixed the issue

--- dank.js
+++ dank.js
@@ -1,4 +1,5 @@
 document.body.innerHTML = document.body.innerHTML.replace(new RegExp("cool", "g"), "dank");
-//document.body.innerHTML = document.body.innerHTML.replace(new RegExp("awesome", "i"), "dank");
-//document.body.innerHTML = document.body.innerHTML.replace(new RegExp("amazing", "i"), "dank");
-//document.body.innerHTML = document.body.innerHTML.replace(new RegExp("and", "i"), "dank");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("awesome", "g"), "dank");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("amazing", "g"), "dank");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("hip", "g"), "dank");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("sweet", "g"), "dank");
