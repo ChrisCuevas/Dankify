57484121f7bd0b3d021d16dfcadd9df73fa59913
ChrisCuevas <ccuevas@wisc.edu>
2015-07-09 23:20:48 -0700

    Updated to replace article titles and body with dank.

--- dank.js
+++ dank.js
@@ -1,5 +1,14 @@
+//replacing text in article body
 document.body.innerHTML = document.body.innerHTML.replace(new RegExp("cool", "g"), "dank");
 document.body.innerHTML = document.body.innerHTML.replace(new RegExp("awesome", "g"), "dank");
 document.body.innerHTML = document.body.innerHTML.replace(new RegExp("amazing", "g"), "dank");
 document.body.innerHTML = document.body.innerHTML.replace(new RegExp("hip", "g"), "dank");
 document.body.innerHTML = document.body.innerHTML.replace(new RegExp("sweet", "g"), "dank");
+
+
+//Replacing text in article titles
+document.title = document.title.replace(new RegExp("cool", "g"), "dank");
+document.title = document.title.replace(new RegExp("awesome", "g"), "dank");
+document.title = document.title.replace(new RegExp("amazing", "g"), "dank");
+document.title = document.title.replace(new RegExp("hip", "g"), "dank");
+document.title = document.title.replace(new RegExp("sweet", "g"), "dank");
