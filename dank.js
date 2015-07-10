8ce0b14d8530be6600581c1a31823e60b5aa472f
ChrisCuevas <ccuevas@wisc.edu>
2015-07-09 23:01:23 -0700

    fixed bug in js

--- dank.js
+++ dank.js
@@ -1,4 +1,4 @@
-document.body.innerHTML = document.body.replace(new RegExp("cool", "i"), "dank");
-document.body.innerHTML = document.body.replace(new RegExp("awesome", "i"), "dank");
-document.body.innerHTML = document.body.replace(new RegExp("amazing", "i"), "dank");
-document.body.innerHTML = document.body.replace(new RegExp("and", "i"), "dank");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("cool", "i"), "dank");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("awesome", "i"), "dank");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("amazing", "i"), "dank");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("and", "i"), "dank");
